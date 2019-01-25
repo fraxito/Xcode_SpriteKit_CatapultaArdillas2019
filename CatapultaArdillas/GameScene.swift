5a26144d14eedbd5f72c2d6213bfc5b90e83a6d8
administrador <administrador@maclabserver.local>
2019-01-25 16:37:20 +0100

    Finalizado a Viernes 25-1

--- CatapultaArdillas/GameScene.swift
+++ CatapultaArdillas/GameScene.swift
@@ -9,81 +9,118 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var catapulta = SKSpriteNode()
+    var giroIzq = SKAction()
     
+    var cargadorNivel : SKNode!
+    var salidaBellotas : SKNode!
+    var disparoCamara = SKSpriteNode()
+    
+    
+ 
     override func didMove(to view: SKView) {
+       catapulta = childNode(withName: "catapulta") as! SKSpriteNode
+        giroIzq = SKAction.rotate(toAngle: 45, duration: 0.5, shortestUnitArc:true)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        cargadorNivel = childNode(withName: "cargadorNivel")
+        cargadorNivel.addChild(SKReferenceNode(fileNamed:"Nivel1.sks"))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        salidaBellotas = childNode(withName: "salidaBellotas")
+        creaMuelle()
+        creaCazo()
+        self.physicsWorld.contactDelegate = self
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        catapulta.removeAllActions()
+        catapulta.run(giroIzq)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        //creo un disparo y lo añado en el punto de salida de las bellotas
+        let disparo = SKSpriteNode(imageNamed: "acorn2.png")
+        disparo.zPosition = 4
+        disparo.physicsBody = SKPhysicsBody(circleOfRadius: 15)
+        disparo.position = salidaBellotas.position
+        disparo.physicsBody?.collisionBitMask = 3  //0011
+        disparo.physicsBody?.contactTestBitMask = 3 // 0011
+        disparo.physicsBody?.categoryBitMask = 3    //0011
+        
+        disparoCamara = disparo
+        addChild(disparo)
+        self.camera?.position.x = UIScreen.main.bounds.width / 2
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        catapulta.removeAllActions()
+        
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if disparoCamara.position.x > 512{
+            self.camera?.position.x = disparoCamara.position.x
+        }
     }
+    
+    func creaMuelle(){
+        let tamano = CGSize(width: 1, height: 1)
+        //defino un punto de anclaje para el muelle y lo agrego a la pantalla
+        let anclajeMuelle = SKSpriteNode(color: .red, size: tamano)
+        anclajeMuelle.physicsBody = SKPhysicsBody(rectangleOf: tamano)
+        anclajeMuelle.physicsBody?.isDynamic = false
+        anclajeMuelle.position = CGPoint(x: catapulta.position.x, y: catapulta.position.y + 300)
+        
+        anclajeMuelle.physicsBody?.collisionBitMask = 8   // 1000
+        addChild(anclajeMuelle)
+        
+        //creo el muelle y lo añado
+        
+        let muelle = SKPhysicsJointSpring.joint(withBodyA: anclajeMuelle.physicsBody!, bodyB: catapulta.physicsBody!, anchorA: anclajeMuelle.position, anchorB: anclajeMuelle.position)
+        muelle.frequency = 1   //fuerza del muelle
+        muelle.damping = 0.2   //rebote del muelle
+        scene?.physicsWorld.add(muelle)
+        
+    }
+    
+    func creaCazo(){
+        let cazo = childNode(withName: "cazo") as! SKSpriteNode
+        let fijaCazo = SKPhysicsJointFixed.joint(withBodyA: cazo.physicsBody!, bodyB: catapulta.physicsBody!, anchor: cazo.position)
+        
+        scene?.physicsWorld.add(fijaCazo)
+    }
+    
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if (contact.bodyA.categoryBitMask == 3) || (contact.bodyB.categoryBitMask == 3){
+            if (contact.bodyA.categoryBitMask == 5) || (contact.bodyB.categoryBitMask == 5){ //es un gato
+                print ("contacto sucedido entre un gato y una bellota")
+                let emisor = SKEmitterNode(fileNamed: "Explosion.sks")
+                emisor?.position = contact.contactPoint
+                addChild(emisor!)
+                contact.bodyA.node?.removeFromParent()
+                contact.bodyB.node?.removeFromParent()
+            }
+            if (contact.bodyA.categoryBitMask == 6) || (contact.bodyB.categoryBitMask == 6){  //es un perro
+                print ("contacto sucedido entre un perro y una bellota")
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
